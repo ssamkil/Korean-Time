7ce683ef2b61e2f62b31d06db60dbd303c8a237e
7sungcola <donghwa2428@gmail.com>
2024-04-16 18:48:11 +0900

    screen change function added

--- app/src/main/java/com/example/koreantime/MainActivity.kt
+++ app/src/main/java/com/example/koreantime/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.koreantime
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,22 +14,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.koreantime.ui.theme.KoreanTimeTheme
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.koreantime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        /*setContent {
-            KoreanTimeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }*/
-        setContentView(R.layout.activity_main)
+        val btn_register = binding.register
+        val btn_login    = binding.login
+
+        val intent = Intent(this, RegisterActivity::class.java)
+        btn_register.setOnClickListener{startActivity(intent)}
+
+
     }
 }
 
--- app/src/main/java/com/example/koreantime/RegisterActivity.kt
+++ app/src/main/java/com/example/koreantime/RegisterActivity.kt
@@ -5,16 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.koreantime.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val btn_create = binding.create
+        val btn_back   = binding.back
+
+        btn_back.setOnClickListener{finish()}
+
     }
 }
